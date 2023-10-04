bed799eb494d58f7c9fd34289d3efc311f884564
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-10-04 11:39:25 +0530

    basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+// Screens
+import Landing from "./screens/Landing.jsx";
+import Login from "./screens/Login.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+
+
+        <Helmet>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+        </Helmet>
+        <Routes>
+          <Route path='/login' element={<><Login /></>} />
+        </Routes>
+        <Routes>
+          <Route path='/' element={<><Landing /></>} />
+        </Routes>
+
+      </Router>
+
     </>
-  )
+  );
 }
 
-export default App
